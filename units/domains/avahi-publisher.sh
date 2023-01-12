e62a4c3dac037eef18bfec84626d735b9c183346
Andrey Sitnik <andrey@sitnik.ru>
2023-01-13 00:01:33 +0100

    Fix publisher script

--- units/domains/avahi-publisher.sh
+++ units/domains/avahi-publisher.sh
@@ -1,18 +1,19 @@
 #!/bin/sh -e
 
 IP=$(ip route get 1 | awk '{print $7;exit}')
-SITES=$(ls /usr/local/etc/sites)
+SITES=$(ls /usr/local/etc/nginx/conf.d/)
 
-if [[ $SITES != "$HOSTNAME.local.conf" ]]; then
-  for file in $SITES; do
-    if [[ $file == *\.local.conf ]]; then
-      domain="$(basename $file .conf)"
-      if [[ $domain != "$HOSTNAME.local" ]]; then
-        avahi-publish -a $domain -R $IP &
-      fi
+for file in $SITES; do
+  if [[ $file == *\.local.conf ]]; then
+    domain="$(basename $file .conf)"
+    if [[ $domain != "$HOSTNAME.local" ]]; then
+      avahi-publish -a $domain -R $IP &
+      published=1
     fi
-  done
+  fi
+done
 
+if [[ $published ]]; then
   wait -n
   exit $?
 fi
