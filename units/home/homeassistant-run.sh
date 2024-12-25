e92c55842b4c8b10cd284ebad042b55db97b3107
Andrey Sitnik <andrey@sitnik.ru>
2024-12-25 15:11:37 +0100

    Simplify venv script for HA and increase security a little

--- units/home/homeassistant-run.sh
+++ units/home/homeassistant-run.sh
@@ -1,38 +1,25 @@
 #!/usr/bin/with-contenv bashio
 
-USER="home"
-GROUP="home"
+# Based on https://github.com/tribut/homeassistant-docker-venv
 
 VENV_PATH="${VENV:-/var/tmp/homeassistant-venv}"
 CONFIG_PATH=/config
 
-#
 # Create virtual environment
-#
 
-bashio::log.info "Initializing venv in $VENV_PATH"
-su "$USER" \
-  -c "python3 -m venv --system-site-packages '$VENV_PATH'"
+python3 -m venv --system-site-packages $VENV_PATH
 
-#
-# Run homeassistant
-#
-
-bashio::log.info "Activating venv"
 . "$VENV_PATH/bin/activate"
 export UV_SYSTEM_PYTHON=false
 
-bashio::log.info "Installing uv into venv"
-uv --version && su "$USER" \
-  -c "uv pip freeze --system|grep ^uv=|xargs uv pip install"
+uv --version && uv pip freeze --system|grep ^uv=|xargs uv pip install
 
-bashio::log.info "Setting new \$HOME"
-HOME="$( getent passwd "$USER" | cut -d: -f6 )"
+HOME="$CONFIG_PATH"
 export HOME
 
 # Everything below should be kept in sync with upstream's
-#   https://github.com/home-assistant/core/blob/dev/rootfs/etc/services.d/home-assistant/run
-cd "$CONFIG_PATH" || bashio::exit.nok "Can't find config folder: $CONFIG_PATH"
+#  https://github.com/home-assistant/core/blob/dev/rootfs/etc/services.d/home-assistant/run
+cd "$CONFIG_PATH"
 
 # Enable mimalloc for Home Assistant Core, unless disabled
 if [[ -z "${DISABLE_JEMALLOC+x}" ]]; then
@@ -40,7 +27,4 @@ if [[ -z "${DISABLE_JEMALLOC+x}" ]]; then
   export MALLOC_CONF="background_thread:true,metadata_thp:auto,dirty_decay_ms:20000,muzzy_decay_ms:20000"
 fi
 
-bashio::log.info "Starting homeassistant"
-exec \
-  s6-setuidgid "$USER" \
-  python3 -m homeassistant --config "$CONFIG_PATH"
+python3 -m homeassistant --config "$CONFIG_PATH"
